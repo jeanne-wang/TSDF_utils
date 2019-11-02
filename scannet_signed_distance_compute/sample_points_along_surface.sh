9483fabdc4b63fa814dbdb5fdb8f2f71a20f8904
Xiaojuan Wang <xiaojwan@student-net-vpn-0475.ethz.ch>
2019-11-02 10:49:21 -0700

    fix indent bug

--- scannet_signed_distance_compute/sample_points_along_surface.sh
+++ scannet_signed_distance_compute/sample_points_along_surface.sh
@@ -2,15 +2,14 @@ MESH_ROOT="/home/xiaojwan/CVPR2020/Experiment/SparseConvNet/exampes/ScanNet_v2/*
 OUTPUT_PATH="/home/xiaojwan/CVPR2020/ScanNet_v2_points_along_surf"
 for MESH_FILE in $(ls ${MESH_ROOT})
 do
-	SCENE_NAME=$(basename ${MESH_FILE})
+  SCENE_NAME=$(basename ${MESH_FILE})
 	SCENE_NAME=${SCENE_NAME//.ply/}
-
-
-    python3.6 sample_points_along_surface.py --mesh_file $MESH_FILE \
-        --density 0 \
-        --num_sample 10000
-        --variance 0.05 \
-        --output_path $OUTPUT_PATH/$SCENE_NAME"_"
+  
+  python3.6 sample_points_along_surface.py --mesh_file $MESH_FILE \
+    --density 0 \
+    --num_sample 10000 \
+    --variance 0.05 \
+    --output_path $OUTPUT_PATH/$SCENE_NAME"_"
 
 done
 
