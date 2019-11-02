d7429b78d64d4d1010ae4f4201d8177a1c0086a3
Xiaojuan Wang <xiaojwan@Xiaojuans-MBP.attlocal.net>
2019-11-01 23:27:01 -0700

    move distance sign estimate code from TSDF/ to scannet_distance_sign_compute

--- scannet_signed_distance_compute/setup.py
+++ scannet_signed_distance_compute/setup.py
@@ -7,9 +7,7 @@ import numpy as np
 sys.argv[1:] = ["build_ext", "--inplace"]
 
 ext_modules = [
-    Extension("tsdf_volume", ["tsdf_volume.pyx"],
-              include_dirs=[np.get_include()]),
-    Extension("color_sdf_volume", ["color_sdf_volume.pyx"],
+    Extension("freespace_volume_from_2D_cameras", ["freespace_volume_from_2D_cameras.pyx"],
     		  include_dirs=[np.get_include()]),
 ]
 
