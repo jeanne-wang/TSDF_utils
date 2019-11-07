733b147278fa0668c85e86c21477a50df1d7513f
xiaojwan <xiaojwan@student.ethz.ch>
2019-11-07 02:25:08 +0100

    add +x to batch scripts

--- distance_sign_estimate_scripts/compute_distance_transform_batch_1.sh
+++ distance_sign_estimate_scripts/compute_distance_transform_batch_1.sh
@@ -1,4 +1,4 @@
-MESH_ROOT="/media/root/data/ScanNet_v2/*/scene*2.ply"
+MESH_ROOT="/media/root/data/ScanNet_v2_data/*/scene*2.ply"
 POINT_ROOT="/home/xiaojwan/CVPR2020/Experiment/ScanNet_v2_points_depthmap_camera_rays_0_1"
 declare -i count=0
 for MESH_FILE in $(ls ${MESH_ROOT})
