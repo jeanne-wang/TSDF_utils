a8f04c4d05d35fa119dbe69d47681373277f23dd
Xiaojuan Wang <xiaojwan@student-net-vpn-1380.ethz.ch>
2019-11-06 16:55:32 -0800

    change path

--- distance_sign_estimate_scripts/compute_distance_transform_batch_1.sh
+++ distance_sign_estimate_scripts/compute_distance_transform_batch_1.sh
@@ -1,5 +1,5 @@
 MESH_ROOT="/media/root/data/ScanNet_v2/*/scene*2.ply"
-POINT_ROOT="/home/xiaojwan/CVPR2020/Experiment/ScanNet_v2_points_depthmap_camera_rays"
+POINT_ROOT="/home/xiaojwan/CVPR2020/Experiment/ScanNet_v2_points_depthmap_camera_rays_0_1"
 declare -i count=0
 for MESH_FILE in $(ls ${MESH_ROOT})
 do
@@ -16,7 +16,7 @@ do
     echo ${SCENE_NAME}
     ./compute_distance_transform_scannet \
         -f ${MESH_FILE} \
-        -p $POINT_ROOT/$SCENE_NAME"_points_depthmap_camera_rays_0_1.dat" \
+        -p $POINT_ROOT/$SCENE_NAME"_points_depthmap_camera_rays.dat" \
         -o "./scannet/"${SCENE_NAME} \
         -v 10000
     echo "==========================="
