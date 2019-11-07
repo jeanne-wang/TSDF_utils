ff8cc0f929aaefe75e02ab9eecb3e3fb3e879235
Xiaojuan Wang <xiaojwan@student-net-vpn-1380.ethz.ch>
2019-11-06 17:30:39 -0800

    change tool path

--- distance_sign_estimate_scripts/compute_distance_transform_batch_3.sh
+++ distance_sign_estimate_scripts/compute_distance_transform_batch_3.sh
@@ -1,5 +1,6 @@
-MESH_ROOT="/media/root/data/ScanNet_v2/*/scene*2.ply"
+MESH_ROOT="/media/root/data/ScanNet_v2_data/*/scene*2.ply"
 POINT_ROOT="/home/xiaojwan/CVPR2020/Experiment/ScanNet_v2_points_depthmap_camera_rays_0_1"
+TOOL_PATH=/home/xiaojwan/CVPR2020/TSDF_utils/scannet_signed_distance_compute/closest-point-in-mesh-for-a-sampled-point
 declare -i count=0
 for MESH_FILE in $(ls ${MESH_ROOT})
 do
@@ -11,13 +12,13 @@ do
         continue
     fi
 
-    mkdir -p "./scannet/"${SCENE_NAME}
+    mkdir -p $TOOL_PATH/scannet/${SCENE_NAME}
     echo "==========================="
     echo ${SCENE_NAME}
-    ./compute_distance_transform_scannet \
+    ./$TOOL_PATH/compute_distance_transform_scannet \
         -f ${MESH_FILE} \
         -p $POINT_ROOT/$SCENE_NAME"_points_depthmap_camera_rays.dat" \
-        -o "./scannet/"${SCENE_NAME} \
+        -o $TOOL_PATH/scannet/${SCENE_NAME} \
         -v 10000
     echo "==========================="
 
