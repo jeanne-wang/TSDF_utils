4465ab6d07af189b2d344916f0464f9371913db4
xiaojwan <xiaojwan@student.ethz.ch>
2019-12-12 20:55:10 +0100

    change path

--- distance_sign_estimate_scripts/compute_distance_transform_batch_1.sh
+++ distance_sign_estimate_scripts/compute_distance_transform_batch_1.sh
@@ -1,6 +1,6 @@
 MESH_ROOT="/home/xiaojwan/CVPR2020/Experiment/SparseConvNet/examples/scannet_denoising/*/scene*2.ply"
-POINT_ROOT="/home/xiaojwan/CVPR2020/Experiment/ScanNet_v2_point_cloud_sigma_0_01"
-TOOL_PATH=/home/xiaojwan/CVPR2020/Experiment/TSDF_utils/scannet_signed_distance_compute/closest-point-in-mesh-for-a-sampled-point
+POINT_ROOT="/home/xiaojwan/CVPR2020/Experiment/ScanNet_v2_point_cloud_sigma_0_05"
+TOOL_PATH=/home/xiaojwan/CVPR2020/TSDF_utils/scannet_signed_distance_compute/closest-point-in-mesh-for-a-sampled-point
 declare -i count=0
 for MESH_FILE in $(ls ${MESH_ROOT})
 do
@@ -17,7 +17,7 @@ do
     echo ${SCENE_NAME}
     $TOOL_PATH/compute_distance_transform_scannet \
         -f ${MESH_FILE} \
-        -p $POINT_ROOT/$SCENE_NAME"_vh_clean_2_sigma_0_01.dat" \
+        -p $POINT_ROOT/$SCENE_NAME"_vh_clean_2_sigma_0_05.dat" \
         -o $TOOL_PATH/scannet/${SCENE_NAME} \
         -v 10000
     echo "==========================="
